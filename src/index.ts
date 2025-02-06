6b52331291741e2bd0192684385f95ba4587861a
koga1020 <koga.shozo1020@gmail.com>
2025-02-06 23:46:56 +0900

    able to match query

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,42 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Client } from '@opensearch-project/opensearch';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const client = new Client({
+  node: 'http://localhost:9200',
+});
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+app.get('/', async (c) => {
+  const health = await client.cluster.health();
+  return c.json(health.body);
+});
+app.get('/search', async (c) => {
+  const category = c.req.query('q')
+  const result = await client.search({
+    index: 'opensearch_dashboards_sample_data_ecommerce',
+    body: {
+      query: {
+        match: {
+          category: {
+            query: 'M',
+            minimum_should_match: '1%'
+          }
+          
+        },
+        
+
+      }
+    }
+  });
+  return c.json(result);
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
